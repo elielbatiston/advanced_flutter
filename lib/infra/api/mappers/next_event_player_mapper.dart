02ff02fdfef30ec96083a5a752035835cf714b10
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-16 13:36:28 -0300

    move NextEventPlayerMapper to its own file

---
+++ lib/infra/api/mappers/next_event_player_mapper.dart
@@ -0,0 +1,15 @@
+import 'package:advanced_flutter/domain/entities/next_event_player.dart';
+import 'package:advanced_flutter/infra/types/api.dart';
+
+class NextEventPlayerMapper {
+  static List<NextEventPlayer> toList(JsonArr arr) => arr.map(NextEventPlayerMapper.toObject).toList();
+
+  static NextEventPlayer toObject(Json json) => NextEventPlayer(
+    id: json['id'],
+    name: json['name'],
+    position: json['position'],
+    photo: json['photo'],
+    isConfirmed: json['isConfirmed'],
+    confirmationDate: DateTime.tryParse(json['confirmationDate'] ?? ''),
+  );
+}
