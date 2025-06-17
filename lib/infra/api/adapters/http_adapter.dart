efd291ace951d3af78f8a0485a7d6b73266ce82e
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-17 07:38:10 -0300

    make HttpAdapter implements HttpGetClient

--- lib/infra/api/adapters/http_adapter.dart
+++ lib/infra/api/adapters/http_adapter.dart
@@ -1,22 +1,19 @@
 import 'dart:convert';
 
 import 'package:advanced_flutter/domain/entities/domain_error.dart';
+import 'package:advanced_flutter/infra/api/clients/http_get_client.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 
 import 'package:dartx/dartx.dart';
 import 'package:http/http.dart';
 
-class HttpAdapter {
+class HttpAdapter implements HttpGetClient {
   final Client client;
 
   HttpAdapter({required this.client});
 
-  Future<T?> get<T>({
-    required String url,
-    Map<String, String>? headers,
-    Map<String, String?>? params,
-    Map<String, String>? queryString,
-  }) async {
+  @override
+  Future<T?> get<T>({ required String url, Map<String, String>? headers, Map<String, String?>? params, Map<String, String>? queryString }) async {
     final allHeaders = (headers ?? {})..addAll({ 'content-type': 'application/json','accept': 'application/json' });
     final uri = _buildUri(url: url, params: params, queryString: queryString);
     final response = await client.get(uri, headers: allHeaders);
