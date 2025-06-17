6be91c8cdc8819e15b76d33b6521384d1934e1c9
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-17 07:53:52 -0300

    apply class modifiers

--- lib/infra/api/repositories/load_next_event_api_repo.dart
+++ lib/infra/api/repositories/load_next_event_api_repo.dart
@@ -5,15 +5,15 @@ import 'package:advanced_flutter/infra/api/clients/http_get_client.dart';
 import 'package:advanced_flutter/infra/api/mappers/next_event_mapper.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 
-class LoadNextEventApiRepository implements LoadNextEventRepository {
+final class LoadNextEventApiRepository implements LoadNextEventRepository {
   final HttpGetClient httpClient;
   final String url;
 
-  LoadNextEventApiRepository({required this.httpClient, required this.url});
+  const LoadNextEventApiRepository({ required this.httpClient, required this.url });
 
   @override
-  Future<NextEvent> loadNextEvent({required String groupId}) async {
-    final json = await httpClient.get<Json>(url: url, params: {"groupId": groupId});
+  Future<NextEvent> loadNextEvent({ required String groupId }) async {
+    final json = await httpClient.get<Json>(url: url, params: { "groupId": groupId });
     if (json == null) throw DomainError.unexpected;
     return NextEventMapper.toObject(json);
   }
