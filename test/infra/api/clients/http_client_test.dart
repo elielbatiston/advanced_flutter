42d6101e9bf9d63f17e17a0d6f34d35560876d8a
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-16 21:51:54 -0300

    ensure HttpClient returns null on 200 with empty response

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -15,7 +15,7 @@ class HttpClient {
 
   HttpClient({required this.client});
 
-  Future<T> get<T>({
+  Future<T?> get<T>({
     required String url,
     Map<String, String>? headers,
     Map<String, String?>? params,
@@ -31,6 +31,7 @@ class HttpClient {
     switch (response.statusCode) {
       case 200:
         {
+          if (response.body.isEmpty) return null;
           final data = jsonDecode(response.body);
           return (T == JsonArr)
               ? data.map<Json>((e) => e as Json).toList()
@@ -173,8 +174,8 @@ void main() {
 
     test('should return a Map', () async {
       final data = await sut.get<Json>(url: url);
-      expect(data['key1'], 'value1');
-      expect(data['key2'], 'value2');
+      expect(data?['key1'], 'value1');
+      expect(data?['key2'], 'value2');
     });
 
     test('should return a List', () async {
@@ -189,8 +190,8 @@ void main() {
         ]
       ''';
       final data = await sut.get<JsonArr>(url: url);
-      expect(data[0]['key'], 'value1');
-      expect(data[1]['key'], 'value2');
+      expect(data?[0]['key'], 'value1');
+      expect(data?[1]['key'], 'value2');
     });
 
     test('should return a Map with List', () async {
@@ -208,9 +209,15 @@ void main() {
         }
       ''';
       final data = await sut.get<Json>(url: url);
-      expect(data['key1'], 'value1');
-      expect(data['key2'][0]['key'], 'value1');
-      expect(data['key2'][1]['key'], 'value2');
+      expect(data?['key1'], 'value1');
+      expect(data?['key2'][0]['key'], 'value1');
+      expect(data?['key2'][1]['key'], 'value2');
+    });
+
+    test('should return null on 200 with empty response', () async {
+      client.responseJson = '';
+      final data = await sut.get(url: url);
+      expect(data, isNull);
     });
   });
 }
