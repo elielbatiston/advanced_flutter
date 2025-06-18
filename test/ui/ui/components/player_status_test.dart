fe76689e76298100d96dc9c0300dc00cdbf1dffb
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-18 08:06:16 -0300

    should present grey color when confirmation is null

--- test/ui/ui/components/player_status_test.dart
+++ test/ui/ui/components/player_status_test.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 final class PlayerStatus extends StatelessWidget {
-  final bool isConfirmed;
+  final bool? isConfirmed;
 
-  const PlayerStatus({required this.isConfirmed, super.key});
+  const PlayerStatus({
+    this.isConfirmed,
+    super.key
+  });
 
-  Color getColor() => isConfirmed ? Colors.teal : Colors.pink;
+  Color getColor() => switch(isConfirmed) {
+    true => Colors.teal,
+    false => Colors.pink,
+    null => Colors.blueGrey
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -23,22 +30,20 @@ final class PlayerStatus extends StatelessWidget {
 
 void main() {
   testWidgets('should present green status', (tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(home: PlayerStatus(isConfirmed: true)),
-    );
-    final decoration =
-        tester.firstWidget<Container>(find.byType(Container)).decoration
-            as BoxDecoration;
+    await tester.pumpWidget(const MaterialApp(home: PlayerStatus(isConfirmed: true)));
+    final decoration = tester.firstWidget<Container>(find.byType(Container)).decoration as BoxDecoration;
     expect(decoration.color, Colors.teal);
   });
 
   testWidgets('should present red status', (tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(home: PlayerStatus(isConfirmed: false)),
-    );
-    final decoration =
-        tester.firstWidget<Container>(find.byType(Container)).decoration
-            as BoxDecoration;
+    await tester.pumpWidget(const MaterialApp(home: PlayerStatus(isConfirmed: false)));
+    final decoration = tester.firstWidget<Container>(find.byType(Container)).decoration as BoxDecoration;
     expect(decoration.color, Colors.pink);
+  });
+
+  testWidgets('should present grey status', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PlayerStatus(isConfirmed: null)));
+    final decoration = tester.firstWidget<Container>(find.byType(Container)).decoration as BoxDecoration;
+    expect(decoration.color, Colors.blueGrey);
   });
 }
