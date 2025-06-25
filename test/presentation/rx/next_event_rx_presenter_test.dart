23032a1a16920d62814d96511219e29c2ddb80ea
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-25 08:17:11 -0300

    make NextEventRxPresenter implements NextEventPresenter

--- test/presentation/rx/next_event_rx_presenter_test.dart
+++ test/presentation/rx/next_event_rx_presenter_test.dart
@@ -12,8 +12,6 @@
 @Timeout(Duration(seconds: 1))
 library;
 
-import 'dart:math';
-
 import 'package:advanced_flutter/domain/entities/next_event.dart';
 import 'package:advanced_flutter/domain/entities/next_event_player.dart';
 import 'package:advanced_flutter/presentation/presenters/next_event_presenter.dart';
@@ -23,16 +21,20 @@ import 'package:rxdart/rxdart.dart';
 
 import '../../helpers/fakers.dart';
 
-final class NextEventRxPresenter {
+final class NextEventRxPresenter implements NextEventPresenter {
   final Future<NextEvent> Function({ required String groupId }) nextEventLoader;
   final nextEventSubject = BehaviorSubject<NextEventViewModel>();
   final isBusySubject = BehaviorSubject<bool>();
 
   NextEventRxPresenter({ required this.nextEventLoader });
 
+  @override
   Stream<NextEventViewModel> get nextEventStream => nextEventSubject.stream;
+
+  @override
   Stream<bool> get isBusyStream => isBusySubject.stream;
 
+  @override
   Future<void> loadNextEvent({ required String groupId, bool isReload = false }) async {
     try {
       if (isReload) isBusySubject.add(true);
