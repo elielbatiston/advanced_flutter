c955b2e4d550f0580cde85c42fa909ec2e2c9fec
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-26 08:49:30 -0300

    ensure CacheManagerAdapter returns null if FileInfo is empty

--- test/infra/cache/adapters/cache_manager_adapter_test.dart
+++ test/infra/cache/adapters/cache_manager_adapter_test.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:file/file.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart' hide FileSystem;
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../mocks/fakers.dart';
@@ -15,12 +16,163 @@ class CacheManagerAdapter {
 
   Future<dynamic> get({ required String key }) async {
     await client.getFileFromCache(key);
+    return null;
   }
 }
 
-class CacheManagerSpy implements BaseCacheManager {
+final class FileSpy implements File {
+  @override
+  File get absolute => throw UnimplementedError();
+
+  @override
+  String get basename => throw UnimplementedError();
+
+  @override
+  Future<File> copy(String newPath) => throw UnimplementedError();
+
+  @override
+  File copySync(String newPath) => throw UnimplementedError();
+
+  @override
+  Future<File> create({bool recursive = false, bool exclusive = false}) => throw UnimplementedError();
+
+  @override
+  void createSync({bool recursive = false, bool exclusive = false}) => throw UnimplementedError();
+
+  @override
+  Future<FileSystemEntity> delete({bool recursive = false}) => throw UnimplementedError();
+
+  @override
+  void deleteSync({bool recursive = false}) => throw UnimplementedError();
+
+  @override
+  String get dirname => throw UnimplementedError();
+
+  @override
+  Future<bool> exists() => throw UnimplementedError();
+
+  @override
+  bool existsSync() => throw UnimplementedError();
+
+  @override
+  FileSystem get fileSystem => throw UnimplementedError();
+
+  @override
+  bool get isAbsolute => throw UnimplementedError();
+
+  @override
+  Future<DateTime> lastAccessed() => throw UnimplementedError();
+
+  @override
+  DateTime lastAccessedSync() => throw UnimplementedError();
+
+  @override
+  Future<DateTime> lastModified() => throw UnimplementedError();
+
+  @override
+  DateTime lastModifiedSync() => throw UnimplementedError();
+
+  @override
+  Future<int> length() => throw UnimplementedError();
+
+  @override
+  int lengthSync() => throw UnimplementedError();
+
+  @override
+  Future<RandomAccessFile> open({FileMode mode = FileMode.read}) => throw UnimplementedError();
+
+  @override
+  Stream<List<int>> openRead([int? start, int? end]) => throw UnimplementedError();
+
+  @override
+  RandomAccessFile openSync({FileMode mode = FileMode.read}) => throw UnimplementedError();
+
+  @override
+  IOSink openWrite({FileMode mode = FileMode.write, Encoding encoding = utf8}) => throw UnimplementedError();
+
+  @override
+  Directory get parent => throw UnimplementedError();
+
+  @override
+  String get path => throw UnimplementedError();
+
+  @override
+  Future<Uint8List> readAsBytes() => throw UnimplementedError();
+
+  @override
+  Uint8List readAsBytesSync() => throw UnimplementedError();
+
+  @override
+  Future<List<String>> readAsLines({Encoding encoding = utf8}) => throw UnimplementedError();
+
+  @override
+  List<String> readAsLinesSync({Encoding encoding = utf8}) => throw UnimplementedError();
+
+  @override
+  Future<String> readAsString({Encoding encoding = utf8}) => throw UnimplementedError();
+
+  @override
+  String readAsStringSync({Encoding encoding = utf8}) => throw UnimplementedError();
+
+  @override
+  Future<File> rename(String newPath) => throw UnimplementedError();
+
+  @override
+  File renameSync(String newPath) => throw UnimplementedError();
+
+  @override
+  Future<String> resolveSymbolicLinks() => throw UnimplementedError();
+
+  @override
+  String resolveSymbolicLinksSync() => throw UnimplementedError();
+
+  @override
+  Future setLastAccessed(DateTime time) => throw UnimplementedError();
+
+  @override
+  void setLastAccessedSync(DateTime time) {
+  }
+
+  @override
+  Future setLastModified(DateTime time) => throw UnimplementedError();
+
+  @override
+  void setLastModifiedSync(DateTime time) {
+  }
+
+  @override
+  Future<FileStat> stat() => throw UnimplementedError();
+
+  @override
+  FileStat statSync() => throw UnimplementedError();
+
+  @override
+  Uri get uri => throw UnimplementedError();
+
+  @override
+  Stream<FileSystemEvent> watch({int events = FileSystemEvent.all, bool recursive = false}) => throw UnimplementedError();
+
+  @override
+  Future<File> writeAsBytes(List<int> bytes, {FileMode mode = FileMode.write, bool flush = false}) => throw UnimplementedError();
+
+  @override
+  void writeAsBytesSync(List<int> bytes, {FileMode mode = FileMode.write, bool flush = false}) {
+  }
+
+  @override
+  Future<File> writeAsString(String contents, {FileMode mode = FileMode.write, Encoding encoding = utf8, bool flush = false}) => throw UnimplementedError();
+
+  @override
+  void writeAsStringSync(String contents, {FileMode mode = FileMode.write, Encoding encoding = utf8, bool flush = false}) {
+  }
+}
+
+final class CacheManagerSpy implements BaseCacheManager {
   int getFileFromCacheCallsCount = 0;
   String? key;
+  bool _isFileInfoEmpty = false;
+
+  void simulateEmptyFileInfo() => _isFileInfoEmpty = true;
 
   @override
   Future<void> dispose() => throw UnimplementedError();
@@ -38,7 +190,7 @@ class CacheManagerSpy implements BaseCacheManager {
   Future<FileInfo?> getFileFromCache(String key, {bool ignoreMemCache = false}) async {
     getFileFromCacheCallsCount++;
     this.key = key;
-    return null;
+    return _isFileInfoEmpty ? null : FileInfo(FileSpy(), FileSource.Cache, DateTime.now(), '');
   }
 
   @override
@@ -75,5 +227,11 @@ void main() {
     await sut.get(key: key);
     expect(client.key, key);
     expect(client.getFileFromCacheCallsCount, 1);
+  });
+
+  test('should return null if FileInfo is empty', () async {
+    client.simulateEmptyFileInfo();
+    final json = await sut.get(key: key);
+    expect(json, isNull);
   });
 }
