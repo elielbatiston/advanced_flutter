6ce8521adceedb8ce2ba95d823215d0330392d09
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-26 08:53:35 -0300

    ensure CacheManagerAdapter returns null if cache is old

--- test/infra/cache/adapters/cache_manager_adapter_test.dart
+++ test/infra/cache/adapters/cache_manager_adapter_test.dart
@@ -171,8 +171,11 @@ final class CacheManagerSpy implements BaseCacheManager {
   int getFileFromCacheCallsCount = 0;
   String? key;
   bool _isFileInfoEmpty = false;
+  DateTime _validTill = DateTime.now().add(const Duration(seconds: 2));
 
   void simulateEmptyFileInfo() => _isFileInfoEmpty = true;
+
+  void simulateCacheOld() => _validTill = DateTime.now().subtract(const Duration(seconds: 2));
 
   @override
   Future<void> dispose() => throw UnimplementedError();
@@ -190,7 +193,7 @@ final class CacheManagerSpy implements BaseCacheManager {
   Future<FileInfo?> getFileFromCache(String key, {bool ignoreMemCache = false}) async {
     getFileFromCacheCallsCount++;
     this.key = key;
-    return _isFileInfoEmpty ? null : FileInfo(FileSpy(), FileSource.Cache, DateTime.now(), '');
+    return _isFileInfoEmpty ? null : FileInfo(FileSpy(), FileSource.Cache, _validTill, '');
   }
 
   @override
@@ -231,6 +234,12 @@ void main() {
 
   test('should return null if FileInfo is empty', () async {
     client.simulateEmptyFileInfo();
+    final json = await sut.get(key: key);
+    expect(json, isNull);
+  });
+
+  test('should return null if cache is old', () async {
+    client.simulateCacheOld();
     final json = await sut.get(key: key);
     expect(json, isNull);
   });
