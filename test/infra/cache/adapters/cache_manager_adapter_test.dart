a55c4442950b66fc41254be12cffef628de78716
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-26 08:58:27 -0300

    ensure CacheManagerAdapter calls file.exists only once

--- test/infra/cache/adapters/cache_manager_adapter_test.dart
+++ test/infra/cache/adapters/cache_manager_adapter_test.dart
@@ -15,12 +15,15 @@ class CacheManagerAdapter {
   });
 
   Future<dynamic> get({ required String key }) async {
-    await client.getFileFromCache(key);
+    final info = await client.getFileFromCache(key);
+    await info?.file.exists();
     return null;
   }
 }
 
 final class FileSpy implements File {
+  int existsCallsCount = 0;
+
   @override
   File get absolute => throw UnimplementedError();
 
@@ -49,7 +52,10 @@ final class FileSpy implements File {
   String get dirname => throw UnimplementedError();
 
   @override
-  Future<bool> exists() => throw UnimplementedError();
+  Future<bool> exists() async {
+    existsCallsCount++;
+    return true;
+  }
 
   @override
   bool existsSync() => throw UnimplementedError();
@@ -170,6 +176,7 @@ final class FileSpy implements File {
 final class CacheManagerSpy implements BaseCacheManager {
   int getFileFromCacheCallsCount = 0;
   String? key;
+  FileSpy file = FileSpy();
   bool _isFileInfoEmpty = false;
   DateTime _validTill = DateTime.now().add(const Duration(seconds: 2));
 
@@ -193,7 +200,7 @@ final class CacheManagerSpy implements BaseCacheManager {
   Future<FileInfo?> getFileFromCache(String key, {bool ignoreMemCache = false}) async {
     getFileFromCacheCallsCount++;
     this.key = key;
-    return _isFileInfoEmpty ? null : FileInfo(FileSpy(), FileSource.Cache, _validTill, '');
+    return _isFileInfoEmpty ? null : FileInfo(file, FileSource.Cache, _validTill, '');
   }
 
   @override
@@ -242,5 +249,11 @@ void main() {
     client.simulateCacheOld();
     final json = await sut.get(key: key);
     expect(json, isNull);
+  });
+
+  test('should call file.exists only once', () async {
+    await sut.get(key: key);
+    expect(client.key, key);
+    expect(client.file.existsCallsCount, 1);
   });
 }
