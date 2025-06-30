47cec39de984fb40168e0f528d9e3a6b0e1662ce
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-30 08:45:05 -0300

    change LoadNextEventFromApiWithCacheFallbackRepository to use given mapper

--- test/infra/mocks/mapper_spy.dart
+++ test/infra/mocks/mapper_spy.dart
@@ -1,10 +1,15 @@
 import 'package:advanced_flutter/infra/mappers/mapper.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 
+import '../../mocks/fakers.dart';
+
 final class MapperSpy<Dto> implements Mapper<Dto> {
   Json? toDtoInput;
   int toDtoInputCallsCount = 0;
+  int toJsonInputCallsCount = 0;
   Dto toDtoOutput;
+  Dto? toJsonInput;
+  Json toJsonOutput = anyJson();
 
   MapperSpy({
     required this.toDtoOutput
@@ -18,5 +23,9 @@ final class MapperSpy<Dto> implements Mapper<Dto> {
   }
 
   @override
-  Json toJson(Dto dto) => throw UnimplementedError();
+  Json toJson(Dto dto) {
+    toJsonInput = dto;
+    toJsonInputCallsCount++;
+    return toJsonOutput;
+  }
 }
