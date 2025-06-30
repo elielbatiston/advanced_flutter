2ad33151c0907efe19c0b745be529ab5eacd0487
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-30 07:40:24 -0300

    ensure NextEventPlayerMapper map dto to json with empty fields

--- test/infra/mappers/next_event_player_mapper_test.dart
+++ test/infra/mappers/next_event_player_mapper_test.dart
@@ -64,4 +64,19 @@ void main() {
     expect(json['isConfirmed'], dto.isConfirmed);
     expect(json['confirmationDate'], "2024-08-29T13:00:00.000");
   });
+
+  test('should map to json with empty fields', () {
+    final dto = NextEventPlayer(
+      id: anyString(),
+      name: anyString(),
+      isConfirmed: anyBool()
+    );
+    final json = sut.toJson(dto);
+    expect(json['id'], dto.id);
+    expect(json['name'], dto.name);
+    expect(json['position'], isNull);
+    expect(json['photo'], isNull);
+    expect(json['isConfirmed'], dto.isConfirmed);
+    expect(json['confirmationDate'], isNull);
+  });
 }
