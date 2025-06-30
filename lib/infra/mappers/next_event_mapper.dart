8cf79fe18911064727c522df48f29f5c0993e131
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2025-06-30 08:12:24 -0300

    ensure NextEventMapper map dto to json

--- lib/infra/mappers/next_event_mapper.dart
+++ lib/infra/mappers/next_event_mapper.dart
@@ -1,7 +1,6 @@
 import 'package:advanced_flutter/domain/entities/next_event.dart';
 import 'package:advanced_flutter/domain/entities/next_event_player.dart';
 import 'package:advanced_flutter/infra/mappers/mapper.dart';
-import 'package:advanced_flutter/infra/mappers/next_event_player_mapper.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 
 final class NextEventMapper implements Mapper<NextEvent> {
